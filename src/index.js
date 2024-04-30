b640490b4f735f681da84c0351ab061a098e7d4b
hyorimhan <151856914+hyorimhan@users.noreply.github.com>
2024-04-30 23:20:42 +0900

    map함수 간략하게 수정, 주석 추가

--- src/index.js
+++ src/index.js
@@ -9,39 +9,32 @@ fetch(url)
     ); // en 영화만 가져오기
 
     let card_list = movie
-      .map((a) => {
-        const movieTitle = a['original_name'].toLowerCase();
-        const movieOverview = a['overview'];
-        const movieVote = a['vote_average'].toFixed(2);
-        const movieImg = `https://image.tmdb.org/t/p/original${a['poster_path']}`;
-        const movieId = a['id'];
-
+      .map(({ original_name, overview, vote_average, poster_path, id }) => { // movie에 있던 목록 가져와서 html card_list에 넣기
         return `<div class="movie-card"> 
-      <img src="${movieImg}" alt="poster" onclick="alert(${movieId})"/>
-      <h2 class="movie-title">${movieTitle}</h2>
-      <p class="movie-overview">${movieOverview}</p>
-      <p class="movie-vote">⭐️${movieVote}</p>
-    </div>`;
-      })
-      .join(''); //각 영화 카드 담은 html 생성
+      <img src="https://image.tmdb.org/t/p/original${poster_path}" alt="poster" onclick="alert(${id})"/>
+      <h2 class="movie-title">${original_name.toLowerCase()}</h2>  
+      <p class="movie-overview">${overview}</p>
+      <p class="movie-vote">⭐️${vote_average.toFixed(2)}</p>
+    </div>`; // original name은 소문자로 변환, 별점은 소수점 자르기
+      }).join('');
     document.querySelector('.movie-list').innerHTML = card_list;
 
-    document //버튼 클릭시 검색 기능
-      .querySelector('#search-btn')
-      .addEventListener('click', function (event) {
-        event.preventDefault();
-        let inputValue = document
-          .querySelector('#input-text')
-          .value.toLowerCase();
-        inputValue.trim() === '' ? alert('검색어를 입력해주세요') : showResult(inputValue);
-      });
+
+
+    document.querySelector('#search-btn').addEventListener('click', e => {
+      e.preventDefault();
+      const inputValue = document.querySelector('#input-text').value.toLowerCase(); // 검색 버튼 클릭시 검색어 소문자로 변환해 가져오기
+      inputValue.trim() === '' ? alert('검색어를 입력해주세요') : showResult(inputValue); // 검색어가 없으면 alert 띄우고 있으면 showResult 함수 실행
+    });
 
     function showResult(inputValue) {
-      let cards = document.querySelectorAll('.movie-card'); // 모든 영화 카드 선택
+      let cards = document.querySelectorAll('.movie-card'); // 모든 영화 카드 선택해 card 변수 만듦
 
-      cards.forEach((a) => {
-        let movieName = a.querySelector('.movie-title').textContent.toLowerCase(); // 각 카드에서 제목 선택
-        if (movieName.includes(inputValue)) { a.style.display = 'block'; } else { a.style.display = 'none'; }
+      cards.forEach(a => {
+        let movieName = a.querySelector('.movie-title').textContent.toLowerCase(); // card 변수에서 각 카드에서 제목 선택
+        if (movieName.includes(inputValue)) { // 제목에 검색어가 있는지 확인
+          a.style.display = 'block'; // 있으면 보이기
+        } else { a.style.display = 'none'; } // 없으면 가리기
       });
     }
   })
